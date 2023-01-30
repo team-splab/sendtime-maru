027d39594fc8c48a0dc8a3e3b69861fcdbf4eff2
min-uuu <minuuu03@gmail.com>
2023-01-30 18:14:52 +0900

    codes update

---
+++ script.js
@@ -0,0 +1,7 @@
+function redirecting() {
+    window.location.href = "https://www.sendtime.io/reservation?i=r0rnQh";
+}
+
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", redirecting);
