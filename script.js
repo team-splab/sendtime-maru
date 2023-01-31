88ab3a9cd315b7e3bdb48b1d5287afc73f7f6d7f
min-uuu <minuuu03@gmail.com>
2023-01-31 12:04:11 +0900

    입력창을 다 입력해야만 제출할 수 있도록! & 로딩창 추가

--- script.js
+++ script.js
@@ -1,24 +1,71 @@
+function submit() {
+    inputCompany = document.getElementById("company").value;
+    inputName = document.getElementById("name").value;
+    inputEmail = document.getElementById("email").value;
+    inputPhone = document.getElementById("phone").value;
+    inputDocu = document.getElementById("docu").value;
+    inputBusinessTheme = document.getElementById("business-theme").value;
+    inputNetworkingTheme = document.getElementById("networking-theme").value;
+
+    if (
+        inputCompany == "" ||
+        inputName == "" ||
+        inputEmail == "" ||
+        inputPhone == "" ||
+        ((inputPurpose == "business" || inputPurpose == "fund") &&
+            inputDocu == "") ||
+        ((inputPurpose == "business" || inputPurpose == "fund") &&
+            inputBusinessTheme == "") ||
+        ((inputPurpose == "business" || inputPurpose == "fund") &&
+            inputBusinessTheme == "") ||
+        (inputPurpose == "networking" && inputNetworkingTheme == "")
+    ) {
+        document.getElementById("submit-disabled").style.display = "";
+        document.getElementById("submit-abled").style.display = "none";
+    } else {
+        document.getElementById("submit-disabled").style.display = "none";
+        document.getElementById("submit-abled").style.display = "";
+    }
+}
+
 function redirecting() {
-    window.location.href = reservationPageURL;
-    let inputName = document.getElementById("name").value;
-    let inputEmail = document.getElementById("email").value;
-    let inputCompany = document.getElementById("company").value;
-    let inputMenu = document.getElementById("menu").value;
-    let inputGoHome = document.getElementById("go-home").value;
-    let inputCoffee = document.getElementById("coffee").value;
+    inputCompany = document.getElementById("company").value;
+    inputName = document.getElementById("name").value;
+    inputEmail = document.getElementById("email").value;
+    inputPhone = document.getElementById("phone").value;
+    inputDocu = document.getElementById("docu").value;
+    inputBusinessTheme = document.getElementById("business-theme").value;
+    inputNetworkingTheme = document.getElementById("networking-theme").value;
 
     console.log(reservationPageURL);
+    console.log(inputCompany);
     console.log(inputName);
     console.log(inputEmail);
-    console.log(inputCompany);
-    console.log(inputMenu);
-    console.log(inputGoHome);
-    console.log(inputCoffee);
+    console.log(inputPhone);
+    console.log(inputPurpose);
+    console.log(inputDocu);
+    console.log(inputBusinessTheme);
+    console.log(inputNetworkingTheme);
+    console.log(inputPrivate);
+
+    document.getElementsByClassName("loadingPopup")[0].style.display = "block";
+    window.location.href = reservationPageURL;
 }
+
+let inputPurpose = "business";
+let inputPrivate = "yse";
+let inputCompany;
+let inputName;
+let inputEmail;
+let inputPhone;
+let inputDocu;
+let inputBusinessTheme;
+let inputNetworkingTheme;
 
 function getRadioItem(event) {
     purpose = event.target.value;
     console.log(purpose);
+    inputPurpose = purpose;
     if (purpose == "business" || purpose == "fund") {
         document.getElementById("docu-item").style.display = "";
         document.getElementById("business-theme-item").style.display = "";
@@ -30,7 +77,13 @@ function getRadioItem(event) {
     }
 }
 
-const submitButton = document.getElementById("submit-button");
+function getPrivateItem(event) {
+    private = event.target.value;
+    console.log(private);
+    inputPrivate = private;
+}
+
+const submitButton = document.getElementById("submit-button-abled");
 submitButton.addEventListener("click", redirecting);
 
 document.getElementById("networking-theme-item").style.display = "none";
@@ -42,6 +95,9 @@ reservationPageID = urlSearch.get("id");
 reservationPageName = urlSearch.get("name");
 console.log(reservationPageName);
 document.getElementById("mentor-name").innerHTML = reservationPageName;
+
+document.getElementById("submit-disabled").style.display = "";
+document.getElementById("submit-abled").style.display = "none";
 
 reservationPageURL =
     "https://www.sendtime.io/reservation?i=" + reservationPageID;
